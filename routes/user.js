66356a10313ae90bdf3f3e809c7bf931edf0cb5e
Bilalbasheer100 <mohammedbilalbasheer@gmail.com>
2022-08-19 21:06:32 +0530

    image will change when we edit

--- routes/user.js
+++ routes/user.js
@@ -36,6 +36,10 @@ router.get('/signup',(req,res)=>{
 router.post('/signup',(req,res)=>{
   userHelpers.doSignup(req.body).then((response)=>{
     console.log(response);
+    req.session.loggedIn=true
+    req.session.user=response
+    res.redirect('/')
+
   })
 })
    router.post('/login',(req,res)=>{
