87e97c3e5d4cf985f336bc62a6016ce064c9fa46
Bilalbasheer100 <mohammedbilalbasheer@gmail.com>
2022-08-30 08:36:47 +0530

    part 27 47 min with errors

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -215,5 +215,40 @@ module.exports={
 
         })
          
+     },
+     placeOrder:(order,products,total)=>{
+         return new Promise((resolve,reject)=>{
+             console.log(order,products,total);
+             let status=order['payment-method']==='COD'?'placed':'pending'
+             let orderObj={
+                 deliveryDetails:{
+                     mobile:order.mobile,
+                     address:order.address,
+                     pincode:order.pincode
+
+                 },
+                 userId:objectId(order.userId),
+                 paymentMethod:order['payment-method'],
+                 products:products,
+                 status:status
+             }
+
+             db.get().collection(collection.ORDER_COLLECTION).insertOne(orderObj).then((response)=>{
+                 resolve()
+             })
+
+         })
+
+     },
+    getCartProductList:(userId)=>{
+         return new Promise(async(resolve,reject)=>{
+             console.log(userId);
+             let cart=await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
+             console.log(cart);
+             resolve(cart.products)
+
+
+         })
+
      }
     }
