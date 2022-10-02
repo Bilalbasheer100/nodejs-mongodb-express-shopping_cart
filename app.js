cdec60361bb0fc66fe8ecc0b5e8e278b8d5259fc
Bilalbasheer100 <mohammedbilalbasheer@gmail.com>
2022-10-02 11:59:04 +0530

    minor corrections

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
@@ -16,7 +17,7 @@ var session=require('express-session')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials/'}))
+app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials/',handlebars:allowInsecurePrototypeAccess(Handlebars)}));
 
 app.use(logger('dev'));
 app.use(express.json());
